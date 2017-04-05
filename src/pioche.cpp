6797e42cc5281f6e96976cb168ce5f542686464e
Boupi <robin.delaye@live.fr>
2017-04-05 14:45:12 +0200

    pioche

--- src/pioche.cpp
+++ src/pioche.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
 #include <cstdio>
 #include <cstdlib>
+#include <ctime>
 #include <math.h>//use .h because without it the compiler dont find it.
-
+#include <cstring>
 #include "pioche.hpp"
 
 using namespace std;
@@ -11,8 +12,8 @@ using namespace std;
 This class is used to initiate the draw system
 */
 void pioche_init(Pioche& pioche, int taille_elt) {
-	DynamicTab *deck = new DynamicTab;
-	deck->ptr = malloc(taille_elt*10);
+	DynamicTab* deck = (DynamicTab*) malloc(sizeof(DynamicTab));
+	deck->ptr = (char*) malloc(taille_elt*10);
 	deck->size = 0;
 	deck->elementSize = taille_elt;
 	deck->capacity = 10;
@@ -20,8 +21,8 @@ void pioche_init(Pioche& pioche, int taille_elt) {
 
 	//cout << "pioche initialisé" << endl;
 
-	DynamicTab *graveyard = new DynamicTab;
-	graveyard->ptr = malloc(taille_elt*10);
+	DynamicTab* graveyard = (DynamicTab*) malloc(sizeof(DynamicTab));
+	graveyard->ptr = (char*) malloc(taille_elt*10);
 	graveyard->size = 0;
 	graveyard->elementSize = taille_elt;
 	graveyard->capacity = 10;
@@ -33,10 +34,9 @@ void pioche_init(Pioche& pioche, int taille_elt) {
 void pioche_defausse(Pioche& pioche, const void* elt) {
 	if(pioche.graveyard->capacity == pioche.graveyard->size){
 		pioche.graveyard->capacity += 10;
-		pioche.graveyard->ptr = realloc(pioche.graveyard->ptr, 
-			(pioche.graveyard->elementSize*pioche.graveyard->capacity)+10);
+		pioche.graveyard->ptr = (char*) realloc(pioche.graveyard->ptr, pioche.graveyard->elementSize * pioche.graveyard->capacity);
 	}
-    memcpy(pioche.graveyard->ptr + (pioche.graveyard->size * pioche.graveyard->elementSize), elt, pioche.graveyard->elementSize);
+    memcpy( pioche.graveyard->ptr + (pioche.graveyard->size * pioche.graveyard->elementSize), elt, pioche.graveyard->elementSize);
     pioche.graveyard->size += 1;
 }
 
@@ -50,19 +50,33 @@ void pioche_pioche(Pioche& pioche, void* target) {
 }
 
 void pioche_melange(Pioche& pioche) {
-  //Psoeudo code
-	int i = 0;
-	void* temp;
-	//change pioche with graveyard ?
-
-	//Shuffle
-	int n = pioche.deck->size;
-	while(n >= 0){
-		i = floor(random() * n--);
-		temp = pioche.deck->ptr[n];
-		pioche.deck->ptr[n] = pioche.deck->ptr[n];
-		pioche.deck->ptr[n] = temp; 
+	while(pioche.deck->size > 0)
+	{
+		pioche_defausse(pioche, pioche.deck->ptr + pioche.deck->elementSize * (pioche.deck->size - 1));
+		pioche.deck->size--;
 	}
+	void* tmp = malloc(pioche.deck->elementSize);
+	srand(time(nullptr));
+	for (int i = pioche.graveyard->size -1; i >=1; i--)
+	{
+		int pos = rand() % (i+1);
+		memcpy(tmp, pioche.graveyard->ptr + (pioche.graveyard->elementSize * i),pioche.graveyard->elementSize);
+		memcpy(pioche.graveyard->ptr + (pioche.graveyard->elementSize * i), pioche.graveyard->ptr + (pioche.graveyard->elementSize * pos), pioche.graveyard->elementSize);
+		memcpy(pioche.graveyard->ptr + (pioche.graveyard->elementSize * pos), tmp, pioche.graveyard->elementSize);
+	}
+    
+	int taille = pioche.graveyard->size;
+    if(pioche.deck->capacity <= pioche.graveyard->size){
+		pioche.deck->capacity = pioche.graveyard->size + 10;
+		pioche.deck->ptr = (char*) realloc(pioche.deck->ptr, pioche.deck->elementSize * pioche.deck->capacity);
+	}
+	for(int i = 0; i < taille; i++)
+	{
+		memcpy(pioche.deck->ptr + (pioche.deck->elementSize * i), pioche.graveyard->ptr + (pioche.graveyard->elementSize * i), pioche.graveyard->elementSize);
+		pioche.deck->size++;
+	}
+    pioche.graveyard->size = 0;
+	free(tmp);
 }
 
 void pioche_suppr(Pioche& pioche) {
